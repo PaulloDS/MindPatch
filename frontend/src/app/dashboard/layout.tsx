0c3cc35fc16a44851c1f49f6879a71d1cf612867
PaulloDS <122841064+PaulloDS@users.noreply.github.com>
2025-06-27 23:29:31 -0300

    fix: layout e client server corrigidos

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,8 +1,8 @@
-import Sidebar from "@/components/dashboard/Sidebar";
 import { Metadata } from "next";
-import { Inter, Noto_Sans } from "next/font/google";
+import { Inter } from "next/font/google";
 import "../globals.css";
 import { SidebarProvider } from "@/context/SidebarContext";
+import ClientLayout from "@/components/dashboard/ClientLayout";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -16,19 +16,16 @@ export const metadata: Metadata = {
 
 export default function DashboardLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <>
-      <SidebarProvider>
-        <div
-          className={`min-h-screen bg-gradient-to-br from-slate-50 via-blue-50/30 to-indigo-50/50 ${inter.variable}`}
-        >
-          <Sidebar />
-          <main className="p-6">{children}</main>
-        </div>
-      </SidebarProvider>
-    </>
+    <SidebarProvider>
+      <div
+        className={`min-h-screen bg-gradient-to-br from-slate-50 via-blue-50/30 to-indigo-50/50 ${inter.variable}`}
+      >
+        <ClientLayout>{children}</ClientLayout>
+      </div>
+    </SidebarProvider>
   );
 }
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -16,41 +16,8 @@ import { Clock, ExternalLink, Menu } from "lucide-react";
 import Image from "next/image";
 
 export default function DashboardPage() {
-  const { setSidebarOpen } = useSidebar();
   return (
     <main className="lg:pl-80">
-      <Button
-        variant="ghost"
-        size="sm"
-        className="lg:hidden"
-        onClick={() => setSidebarOpen(true)}
-      >
-        <Menu className="w-5 h-5" />
-      </Button>
-      <div className="flex justify-between pb-6">
-        <div className="w-[400px] relative md:block">
-          <span className="absolute ml-3 top-[10px]">🔍</span>
-          <Input
-            className="w-full p-5 pl-9 font-medium border-green-700 md:w-[300px] lg:w-[600px]"
-            placeholder="Buscar por desafios, usuários ..."
-            type="text"
-          />
-        </div>
-        <div className="flex items-center space-x-5">
-          <Button className="py-5 rounded-full bg-gradient-to-r from-emerald-400 to-emerald-600 hover:bg-emerald-200 hover:scale-110 transition-transform duration-300 hover:cursor-pointer">
-            ➕ Novo Desafio
-          </Button>
-
-          <Button className="bg-gradient-to-r from-emerald-400 to-emerald-600 py-5 hover:scale-110 transition-transform duration-300 shadow-lg shadow-green-200 hover:cursor-pointer rounded-full">
-            {" "}
-            ➕ Novo Patch
-          </Button>
-          <Button className="bg-blue-400 w-[40px] rounded-full hover:bg-gray-500 hover:cursor-pointer">
-            🔔
-          </Button>
-        </div>
-      </div>
-
       <div className="mb-5 flex justify-between gap-5">
         <Carousel />
         <div className="w-[35%] ">
