f00b860ed0589bac5e6963a67fbdf5639f313fb8
PaulloDS <122841064+PaulloDS@users.noreply.github.com>
2025-07-04 19:48:43 -0300

    feat: filtro de stattus das conquistas adicionado

--- frontend/src/app/dashboard/conquistas/page.tsx
+++ frontend/src/app/dashboard/conquistas/page.tsx
@@ -4,19 +4,42 @@ import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { api } from "@/lib/axios";
 import { AchievementCard } from "@/components/conquistas/AchievementCard";
+import {
+  MenubarShortcut,
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger,
+  MenubarContent,
+  MenubarItem,
+  MenubarSeparator,
+  MenubarSub,
+  MenubarSubTrigger,
+  MenubarSubContent,
+  MenubarCheckboxItem,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+} from "@/components/ui/menubar";
 
 interface Badge {
   id: number;
   nome: string;
   descricao: string;
   iconeURL: string;
-  conquistado: boolean;
-  conquistadoEm: string | null;
+  conquistada: boolean;
 }
 
 export default function ConquistasPage() {
   const [badges, setBadges] = useState<Badge[]>([]);
   const [loading, setLoading] = useState(true);
+  const [filter, setFilter] = useState<
+    "todas" | "conquistadas" | "emAndamento"
+  >("todas");
+
+  const filteredBadges = badges.filter((badge) => {
+    if (filter === "conquistadas") return badge.conquistada;
+    if (filter === "emAndamento") return !badge.conquistada;
+    return true;
+  });
 
   const userId =
     typeof window !== "undefined" ? sessionStorage.getItem("userId") : null;
@@ -25,7 +48,7 @@ export default function ConquistasPage() {
     if (!userId) return;
 
     try {
-      const res = await api.get(`/auth/users/${userId}/conquistas`, {
+      const res = await api.get(`/badges`, {
         withCredentials: true,
       });
       setBadges(res.data);
@@ -44,17 +67,43 @@ export default function ConquistasPage() {
   if (loading) return <p>Carregando conquistas...</p>;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:pl-80">
-      {badges.map((badge) => (
-        <AchievementCard
-          key={badge.id}
-          title={badge.nome}
-          description={badge.descricao}
-          icon={
-            <img src={badge.iconeURL} alt={badge.nome} className="w-10 h-10" />
-          }
-        />
-      ))}
+    <div className="lg:pl-80">
+      <div className="mb-5">
+        <Menubar className="w-fit">
+          <MenubarMenu>
+            <MenubarTrigger className="bg-emerald-500 text-white">
+              Filtrar
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem onClick={() => setFilter("todas")}>
+                Todas
+              </MenubarItem>
+              <MenubarItem onClick={() => setFilter("conquistadas")}>
+                Conquistadas
+              </MenubarItem>
+              <MenubarItem onClick={() => setFilter("emAndamento")}>
+                Em andamento
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {filteredBadges.map((badge) => (
+          <AchievementCard
+            key={badge.id}
+            title={badge.nome}
+            description={badge.descricao}
+            icon={
+              <img
+                src={badge.iconeURL}
+                alt={badge.nome}
+                className="w-10 h-10"
+              />
+            }
+          />
+        ))}
+      </div>
     </div>
   );
 }
