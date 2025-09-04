1c597dbdf87ae30a27b6feb4f1fd4c72ae0b0e43
PaulloDS <122841064+PaulloDS@users.noreply.github.com>
2025-09-03 21:58:53 -0300

    feat: tarefas dos desafios adicionadas

---
+++ backend/src/main/java/mindpatch/backend/model/Tarefa.java
@@ -0,0 +1,38 @@
+package mindpatch.backend.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tarefas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String descricao;
+
+    private boolean concluida = false;
+
+    @ManyToOne
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+}
